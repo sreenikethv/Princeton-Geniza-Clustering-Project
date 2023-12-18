203b337c44457cbc9f4b60ee4a9ee2b8bb4b4f35
sreenikethv <113258064+sreenikethv@users.noreply.github.com>
2023-12-18 16:10:58 -0500

    Create dials.py
    Variables detailing the thresholds for document search

---
+++ dials.py
@@ -0,0 +1,5 @@
+# the dials
+num_neighbors = 5 # number of closest neighbors for each document
+distance_threshold = 0.6 # minimum distance a document needs to be to be included in the cluster
+yellow_distance = 0.8 # medium-distance neighbors
+green_distance = 0.9 # close neighbors
